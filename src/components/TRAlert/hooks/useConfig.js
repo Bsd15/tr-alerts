333d5391ea3f8b08f6a2617848d41220c8b17c7a
bsd <datta.bathina15@gmail.com>
2020-07-20 22:49:49 +0530

    Memoized showAlert() & hideAlert()

--- src/components/TRAlert/hooks/useConfig.js
+++ src/components/TRAlert/hooks/useConfig.js
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
 export const useConfig = () => {
 	const [show, setShow] = useState(false);
 	const initAlertConfig = { message: '', alertType: '', heading: '' };
 	const [alertConfig, setAlertConfig] = useState(initAlertConfig);
 
-	const showAlert = (message, alertType, heading = '') => {
+	const showAlert = useCallback((message, alertType, heading = '') => {
 		if (message) {
 			setAlertConfig({
 				message: message,
@@ -14,12 +14,12 @@ export const useConfig = () => {
 			});
 			setShow(true);
 		}
-	};
+	}, []);
 
-	const hideAlert = () => {
+	const hideAlert = useCallback(() => {
 		setAlertConfig(initAlertConfig);
 		setShow(false);
-	};
+	}, [initAlertConfig]);
 
 	return { show, alertConfig, showAlert, hideAlert };
 };
