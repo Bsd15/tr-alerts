05b5c6b18aec5f7aad9295c0499db304e5a2e16c
bsd <datta.bathina15@gmail.com>
2020-07-20 16:39:40 +0530

    Added propTypes validation for props.children

--- src/components/TRAlert/TRAlert.js
+++ src/components/TRAlert/TRAlert.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AlertContext from './alertContext';
 const TRAlert = (props) => {
 	return <AlertContext.Provider>{props.children}</AlertContext.Provider>;
+};
+
+TRAlert.propTypes = {
+	children: PropTypes.node.isRequired,
 };
 
 export default TRAlert;
