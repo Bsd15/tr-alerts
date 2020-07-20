e4b29fae596283d8417df8f7a6e717e6123a4275
bsd <datta.bathina15@gmail.com>
2020-07-20 21:46:39 +0530

    Imported TRAlert component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import TRAlert from './components/TRAlert/TRAlert';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div className="container">
-      Hello world
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<div className="container">
+			Hello world
+			<TRAlert />
+		</div>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
