53a00d51dcc903f44b52a4df9b98d6455df70726
bsd <datta.bathina15@gmail.com>
2020-07-21 16:53:47 +0530

    Refactored custom hook to use global state that can be share btw components

--- src/components/TRAlert/hooks/useAlert.js
+++ src/components/TRAlert/hooks/useAlert.js
@@ -1,7 +1,24 @@
-import useConfig from './useConfig';
+import { useState, useEffect } from 'react';
+
+let alertConfig = { message: '', alertType: '', heading: '', show: false };
+let listeners = [];
+
 const useAlert = () => {
-	const { showAlert, closeAlert } = useConfig();
-	return { showAlert, closeAlert };
+	const setState = useState(alertConfig)[1];
+	const showAlert = (message, alertType, heading) => {
+		alertConfig = { message, alertType, heading, show: true };
+		listeners.forEach((listener) => {
+			listener(alertConfig);
+		});
+		console.log(alertConfig);
+	};
+	useEffect(() => {
+		listeners.push(setState);
+		return () => {
+			listeners = listeners.filter((listener) => listener !== setState);
+		};
+	}, [setState]);
+	return [alertConfig, showAlert];
 };
 
 export default useAlert;
