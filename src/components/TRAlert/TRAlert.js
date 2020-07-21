3683c47fd64673170fa82f544f4e4cf10c827c82
bsd <datta.bathina15@gmail.com>
2020-07-21 17:04:33 +0530

    Modified onClick function of close button to call closeAlert Used closeAlert from useAlertConfig hook

--- src/components/TRAlert/TRAlert.js
+++ src/components/TRAlert/TRAlert.js
@@ -2,7 +2,7 @@ import React from 'react';
 import useAlertConfig from './hooks/useAlertConfig';
 
 const TRAlert = () => {
-	const [alertConfig, showAlert] = useAlertConfig();
+	const [alertConfig, showAlert, closeAlert] = useAlertConfig();
 	return (
 		<article className="fixed bottom-0 left-0 right-0 mx-auto container w-full lg:w-1/2 p-2 bg-white shadow-2xl border-t-8">
 			<section className="grid grid-row-2" id="content">
@@ -10,12 +10,7 @@ const TRAlert = () => {
 					<div className=" h-8 text-lg font-bold overflow-hidden" id="heading">
 						{alertConfig.heading}
 					</div>
-					<div
-						id="closeButton"
-						onClick={() =>
-							showAlert('Test', 'Alert Type Test', 'Heading', true)
-						}
-					>
+					<div id="closeButton" onClick={() => closeAlert()}>
 						<div className="w-6 h-6">
 							<svg
 								// height="512pt"
