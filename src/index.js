6017b777ac3ddf4a862bd84b24e574cd20961fab
bsd <datta.bathina15@gmail.com>
2020-07-21 16:58:49 +0530

    Removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TRAlert from './components/TRAlert/TRAlert';
-import useAlert from './components/TRAlert/hooks/useAlert';
 import App from './App';
 
 ReactDOM.render(
