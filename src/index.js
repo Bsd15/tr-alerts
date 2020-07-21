4c3bf00644538722bf0b8a7c8691332151a44045
bsd <datta.bathina15@gmail.com>
2020-07-21 15:50:30 +0530

    Added App component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import TRAlert from './components/TRAlert/TRAlert';
+import useAlert from './components/TRAlert/hooks/useAlert';
+import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<div className="container">
-			Hello world
-			<TRAlert />
-		</div>
+		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
